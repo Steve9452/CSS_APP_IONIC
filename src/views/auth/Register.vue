c4a01889ad218afeec298e1e61e031d24ef6ee1e
Steve9452 <00380119@uca.edu.sv>
2024-03-22 11:59:26 -0600

    Fix: endpoint registro

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -162,7 +162,7 @@ export default {
                 }
                 try {
                     const API_ENDOINT = this.getAPIEndpoint();
-                    const request = await fetch(API_ENDOINT + '/', {
+                    const request = await fetch(API_ENDOINT + '/registro', {
                         method: "POST",
                         body: JSON.stringify({
                             correo: correo,
