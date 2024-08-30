5848d934189a28702a37ae7c7e2254a5c0ff2b4c
Steve9452 <00380119@uca.edu.sv>
2024-08-29 20:27:27 -0600

    Buildd android

--- src/mixins/index.ts
+++ src/mixins/index.ts
@@ -7,10 +7,11 @@ const mixin = {
             // return 'http://172.203.229.152/public/api';
             // return 'http://uca-css.test/css-proyecto/public/api'
             // return 'http://192.168.31.220/api'  
-            return 'https://jgdevtech.lat/servicio-social/proyectos/public/api'  
+            // return 'https://jgdevtech.lat/servicio-social/proyectos/public/api'  
+            // return 'http://localhost/api'
             //return 'http://127.0.0.1/api'
             // return 'https://uca.edu.sv/servicio-social/dev-proyectos/public/api'
-            // return 'https://uca.edu.sv/servicio-social/dev-proyectos/public/api'
+            return 'https://uca.edu.sv/servicio-social/proyectos/public/api'
         },
         async setAuthenticatedUser(userData: any) {
             await Storage.set({
